8bc519149f55a9156c636576cbc11b32b4b94ff3
Nazwa-RpL <salsabillahnaswa36@gmail.com>
2025-10-27 07:39:50 +0700

    Migration drop table

--- database/migrations/2025_10_27_002115_drop_address_from_siswas.php
+++ database/migrations/2025_10_27_002115_drop_address_from_siswas.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('siswas', function (Blueprint $table) {
-            //
+            $table->dropColumn('address');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('siswas', function (Blueprint $table) {
-            //
+            $table->string('address');
         });
     }
 };
